fae5f9265caeecd4afcf12a79c3f4b21b7633d73
高橋　宗太 <souta_takahashi@012grp.co.jp>
2020-05-27 11:55:09 +0900

    first commit

---
+++ pages/index.js
@@ -0,0 +1,56 @@
+import Head from "next/head";
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
+import Alert from "../components/alert";
+import Link from "next/link";
+import Date from "../components/date";
+import { getSortedPostsData } from "../lib/posts";
+
+// export default function Home() {
+//   return (
+//     <Layout home>
+//       <Head>
+//         <title>{siteTitle}</title>
+//       </Head>
+//       <section className={utilStyles.headingMd}>
+//         <p>intro</p>
+//       </section>
+//       <Alert type="error">this is error</Alert>
+//       <Alert type="success">this is success</Alert>
+//     </Layout>
+//   );
+// }
+
+export default function Home({ allPostsData }) {
+  return (
+    <Layout home>
+      <Head>…</Head>
+      <section className={utilStyles.headingMd}>…</section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href="/posts/[id]" as={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
+  );
+}
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData
+    }
+  };
+}
